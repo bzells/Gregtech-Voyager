ec787b2df7334df24a9138a310c7956f6a2ee658
TeusTF <tacoeater891444@gmail.com>
2026-04-16 20:06:02 -0500

    Update tag_edits.js
    Edited comments

--- kubejs/server_scripts/tag_edits.js
+++ kubejs/server_scripts/tag_edits.js
@@ -1,4 +1,4 @@
 ServerEvents.tags('block', event => {
-  // Removes the flower_pots tag from potted botania flowers
+  // Removes the flower_pots tag from potted botania flowers to stop generation in dungeons
   event.remove('minecraft:flower_pots', [/botania:potted.*/])
 })
