3b71611b48e7139aabdca20ef763a3032ed3eb60
bzells <derpers1011@gmail.com>
2026-08-22 18:04:53 -0500

    p2

--- kubejs/startup_scripts/recipe_io.js
+++ kubejs/startup_scripts/recipe_io.js
@@ -1,5 +1,5 @@
 GTCEuStartupEvents.registry("gtceu:recipe_type", (_event) => {
-    GTRecipeTypes.BLAST_RECIPES.setMaxIOSize(4, 1, 2, 2)
+    GTRecipeTypes.BLAST_RECIPES.setMaxIOSize(4, 4, 2, 2)
     GTRecipeTypes.CENTRIFUGE_RECIPES.setMaxIOSize(3, 6, 2, 2)
     GTRecipeTypes.VACUUM_RECIPES.setMaxIOSize(1, 1, 2, 2)
     GTRecipeTypes.LARGE_CHEMICAL_RECIPES.setMaxIOSize(6, 6, 4, 4)
